9bf66aaa3c2a3edb085cdfcf0e50b13683dcdd7b
Ashraf Elshaer <ashrafelshaer98@icloud.com>
2024-07-03 19:53:35 -0500

    refactor: Update loading component to include padding

--- apps/dashboard/src/app/(root)/(home)/loading.tsx
+++ apps/dashboard/src/app/(root)/(home)/loading.tsx
@@ -1,6 +1,6 @@
 export default function DashboardLoading() {
 	return (
-		<div className="flex items-center justify-center h-full">
+		<div className="flex items-center justify-center h-full p-4">
 			<div className="flex items-center justify-center">loading ...</div>
 		</div>
 	);
