2bcfe9089722010ff75d00edb206a25310960199
Ashraf Elshaer <ashrafelshaer98@icloud.com>
2024-07-08 11:20:29 -0500

    update supabase ssr

--- packages/supabase/src/client/server.ts
+++ packages/supabase/src/client/server.ts
@@ -23,43 +23,15 @@ export function createServerClient(options: SupabaseClientOptions = {
 
   // Create a server's supabase client with newly configured cookie,
   // which could be used to maintain user's session
-  return _createServerClient<Database>(
+  return _createServerClient(
     env.NEXT_PUBLIC_SUPABASE_URL,
-    options.isAdmin
-      ? env.NEXT_PUBLIC_SUPABASR_SERVICE_ROLE_KEY
-      : env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     {
-      global: {
-        fetch: createFetch({
-          cache: "no-cache",
-          next: {},
-        }),
-      },
       cookies: {
-        get(name: string) {
-          return cookieStore.get(name)?.value;
-        },
-        set(name, value, options) {
-          try {
-            cookieStore.set({ name, value, ...options });
-          } catch (error) {
-            // The `set` method was called from a Server Component.
-            // This can be ignored if you have middleware refreshing
-            // user sessions.
-          }
-        },
-        remove(name, options) {
-          try {
-            cookieStore.set({ name, value: "", ...options });
-          } catch (error) {
-            // The `delete` method was called from a Server Component.
-            // This can be ignored if you have middleware refreshing
-            // user sessions.
-          }
+        getAll() {
+          return cookieStore.getAll();
         },
       },
     },
   );
 }
-
-
