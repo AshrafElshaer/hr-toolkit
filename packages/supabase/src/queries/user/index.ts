3511d4882c0cf8d5c24df6de55e8aee4eb92739e
Ashraf Elshaer <ashrafelshaer98@icloud.com>
2024-07-08 11:22:35 -0500

    refactor: Optimize employee query and improve loading for attendance table

--- packages/supabase/src/queries/user/index.ts
+++ packages/supabase/src/queries/user/index.ts
@@ -20,42 +20,36 @@ export async function getUser(supabase: SupabaseClient) {
   return { user, error };
 }
 
-export const getEmployees = unstable_cache(
-  async (supabase: SupabaseClient) => {
-    const { user } = await getUser(supabase);
-    if (!user || (user.role === "manager" && !user.department_id)) {
-      throw new Error("User not found");
-    }
+export const getEmployees = async (supabase: SupabaseClient) => {
+  const { user } = await getUser(supabase);
+  if (!user || (user.role === "manager" && !user.department_id)) {
+    throw new Error("User not found");
+  }
 
-    if (user.role === "manager") {
-      const { data: employees, error } = await supabase
-        .from("users")
-        .select("*, department:department_id(*)")
-        .eq(
-          "department_id",
-          user.department_id as string,
-        );
-      if (error) {
-        throw error;
-      }
-      return employees;
-    }
-
+  if (user.role === "manager") {
     const { data: employees, error } = await supabase
       .from("users")
-      .select("*, department:department_id(*)");
-
+      .select("*, department:department_id(*)")
+      .eq(
+        "department_id",
+        user.department_id as string,
+      );
     if (error) {
       throw error;
     }
-
     return employees;
-  },
-  ["employees"],
-  {
-    tags: ["employees"],
-  },
-);
+  }
+
+  const { data: employees, error } = await supabase
+    .from("users")
+    .select("*, department:department_id(*)");
+
+  if (error) {
+    throw error;
+  }
+
+  return employees;
+};
 
 export async function getEmployeeById(
   supabase: SupabaseClient,
