72a26fcf83c85788243f2d356d2e77c337e14803
Ashraf Elshaer <ashrafelshaer98@icloud.com>
2024-07-11 11:44:23 -0500

    refactor: Update attendance loading component styling

--- apps/dashboard/src/app/(root)/attendance/loading.tsx
+++ apps/dashboard/src/app/(root)/attendance/loading.tsx
@@ -2,8 +2,8 @@ import Main from "@/components/main";
 import AttendanceFilter from "../employees/[organizationId]/[employeeId]/attendance/_components/attendance-filter";
 export default function AttendanceLoading() {
 	return (
-		<Main className="flex justify-end" maxHeight>
-			<AttendanceFilter />
+		<Main maxHeight>
+			<AttendanceFilter className="items-start " />
 		</Main>
 	);
 }
