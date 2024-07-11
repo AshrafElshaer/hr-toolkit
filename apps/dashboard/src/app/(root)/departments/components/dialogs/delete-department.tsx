b781e8498d46bbef4a5ccc50c34dcd7ee8fe5376
Ashraf Elshaer <ashrafelshaer98@icloud.com>
2024-07-10 21:45:04 -0500

    fix delete dialog footer

--- apps/dashboard/src/app/(root)/departments/components/dialogs/delete-department.tsx
+++ apps/dashboard/src/app/(root)/departments/components/dialogs/delete-department.tsx
@@ -6,113 +6,90 @@ import { toast } from "sonner";
 import type { SupabaseClient } from "@hr-toolkit/supabase/types";
 
 import {
-  AlertDialog,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
+	AlertDialog,
+	AlertDialogCancel,
+	AlertDialogContent,
+	AlertDialogDescription,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogTitle,
 } from "@hr-toolkit/ui/alert-dialog";
 import { Button } from "@hr-toolkit/ui/button";
-import { AnimatePresence, motion } from "framer-motion";
+
 import { Loader } from "lucide-react";
 type Props = {
-  department: {
-    id: string | undefined;
-    name: string | undefined;
-  };
-  onClose: () => void;
-  supabase: SupabaseClient;
-  isDelete: boolean;
-  toggleIsDelete: () => void;
+	department: {
+		id: string | undefined;
+		name: string | undefined;
+	};
+	onClose: () => void;
+	supabase: SupabaseClient;
+	isDelete: boolean;
+	toggleIsDelete: () => void;
 };
 export function DeleteDepartment({
-  department,
-  isDelete,
-  onClose,
-  toggleIsDelete,
+	department,
+	isDelete,
+	onClose,
+	toggleIsDelete,
 }: Props) {
-  const { mutateAsync, isPending } = useMutation({
-    mutationFn: deleteDepartment,
-    onSuccess: () => {
-      queryClient.invalidateQueries({
-        queryKey: ["departments"],
-      });
-      onClose();
-    },
-  });
+	const { mutateAsync, isPending } = useMutation({
+		mutationFn: deleteDepartment,
+		onSuccess: () => {
+			queryClient.invalidateQueries({
+				queryKey: ["departments"],
+			});
+			onClose();
+		},
+	});
 
-  async function handleDelete() {
-    if (!department.id) {
-      return null;
-    }
-    const { data, serverError, validationError } = await mutateAsync({
-      id: department.id as string,
-    });
+	async function handleDelete() {
+		if (!department.id) {
+			return null;
+		}
+		const { data, serverError, validationError } = await mutateAsync({
+			id: department.id as string,
+		});
 
-    if (serverError) {
-      toast.error("An error occurred while deleting the department.", {
-        description: serverError,
-      });
+		if (serverError) {
+			toast.error("An error occurred while deleting the department.", {
+				description: serverError,
+			});
 
-      return;
-    }
-    if (validationError) {
-      toast.error("An error occurred while deleting the department.");
-      return;
-    }
+			return;
+		}
+		if (validationError) {
+			toast.error("An error occurred while deleting the department.");
+			return;
+		}
 
-    toast.success("The department has been deleted successfully.");
-  }
-  return (
-    <AlertDialog open={isDelete} onOpenChange={toggleIsDelete}>
-      <AlertDialogContent>
-        <AlertDialogHeader>
-          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-          <AlertDialogTitle>
-            You wnat to delete {department?.name} department.
-          </AlertDialogTitle>
-          <AlertDialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
-          </AlertDialogDescription>
-        </AlertDialogHeader>
-        <AlertDialogFooter>
-          <AlertDialogCancel>Cancel</AlertDialogCancel>
-          <Button
-            variant={"destructive"}
-            onClick={handleDelete}
-            disabled={isPending}
-          >
-            <AnimatePresence mode="wait" initial={false}>
-              {isPending ? (
-                <motion.span
-                  key="submitting"
-                  initial={{ opacity: 0, y: 10 }}
-                  animate={{ opacity: 1, y: 0 }}
-                  exit={{ opacity: 0, y: -10 }}
-                  transition={{ duration: 0.2 }}
-                  className="flex items-center"
-                >
-                  <Loader className="h-4 w-4 mr-2 animate-spin" />
-                  Deleting ...
-                </motion.span>
-              ) : (
-                <motion.span
-                  key="submit"
-                  initial={{ opacity: 0, y: 10 }}
-                  animate={{ opacity: 1, y: 0 }}
-                  exit={{ opacity: 0, y: -10 }}
-                  transition={{ duration: 0.2 }}
-                >
-                  Delete
-                </motion.span>
-              )}
-            </AnimatePresence>
-          </Button>
-        </AlertDialogFooter>
-      </AlertDialogContent>
-    </AlertDialog>
-  );
+		toast.success("The department has been deleted successfully.");
+	}
+	return (
+		<AlertDialog open={isDelete} onOpenChange={toggleIsDelete}>
+			<AlertDialogContent>
+				<AlertDialogHeader>
+					<AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+					<AlertDialogTitle>
+						You wnat to delete {department?.name} department.
+					</AlertDialogTitle>
+					<AlertDialogDescription>
+						This action cannot be undone. This will permanently delete your
+						account and remove your data from our servers.
+					</AlertDialogDescription>
+				</AlertDialogHeader>
+				<AlertDialogFooter>
+					<AlertDialogCancel>Cancel</AlertDialogCancel>
+					<Button
+						variant={"destructive"}
+						onClick={handleDelete}
+						disabled={isPending}
+					>
+						{isPending && <Loader className="h-4 w-4 mr-2 animate-spin" />}
+						Delete
+					</Button>
+				</AlertDialogFooter>
+			</AlertDialogContent>
+		</AlertDialog>
+	);
 }
