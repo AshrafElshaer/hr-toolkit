08ac1165a2845606cd9eed8e253190cb11d19b9c
Ashraf Elshaer <ashrafelshaer98@icloud.com>
2024-08-24 17:00:36 -0500

    clean up welcome message component

--- apps/dashboard/src/app/(root)/(home)/_components/header/welcome.tsx
+++ apps/dashboard/src/app/(root)/(home)/_components/header/welcome.tsx
@@ -1,35 +1,33 @@
-// import { getEventsByDate } from "@hr-toolkit/supabase/events-queries";
-// import { createServerClient } from "@hr-toolkit/supabase/server";
-// import { getUser } from "@hr-toolkit/supabase/user-queries";
-import UserAvatar from "@/components/user-avatar";
 import { createServerClient } from "@/lib/supabase/server";
 import { getCurrentUser } from "@hr-toolkit/supabase/user-queries";
-import { format } from "date-fns";
-import React from "react";
+import { getEventsByDateRange } from "@hr-toolkit/supabase/events-queries";
+import { calendarSearchParamsCache } from "../calendar/calendar-search-params";
 
 export default async function WelcomeMessage() {
+	const { from, to } = calendarSearchParamsCache.all();
 	const supabase = createServerClient();
-	const { user } = await getCurrentUser(supabase);
-	//   const userPromise = getUser(supabase);
-	//   const eventsPromise = getEventsByDate(supabase, {
-	//     from: format(new Date(), "yyyy-MM-dd"),
-	//     to: format(new Date(), "yyyy-MM-dd"),
-	//   });
 
-	//   const [{ user }, { data: events }] = await Promise.all([
-	//     userPromise,
-	//     eventsPromise,
-	//   ]);
+	const userPromise = getCurrentUser(supabase);
+	const eventsPromise = getEventsByDateRange(supabase, {
+		from,
+		to,
+	});
 
-	//   const eventsCount = events?.length ?? 0;
+	const [{ user }, { data: events }] = await Promise.all([
+		userPromise,
+		eventsPromise,
+	]);
+
+	const eventsCount = events?.length ?? 0;
 
 	return (
 		<div className="flex flex-col gap-2 justify-center lg:col-span-3">
 			<h1 className="text-xl md:text-2xl font-semibold">
-				Welcome back, {user?.first_name} 👋 
+				Welcome back, {user?.first_name} 👋
 			</h1>
 			<p className="text-sm text-secondary-foreground/70">
-				You have 0 upcoming events , 0 pending requests, and 5 unread messages.
+				You have {eventsCount} upcoming events , 0 pending requests, and 5
+				unread messages.
 			</p>
 		</div>
 	);
