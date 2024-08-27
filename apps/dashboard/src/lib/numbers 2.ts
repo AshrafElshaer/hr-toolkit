9719be8dc8fb24d81882a8805f05587575214b94
Ashraf Elshaer <ashrafelshaer98@icloud.com>
2024-08-27 07:37:36 -0500

    fix build errors

--- apps/dashboard/src/lib/numbers 2.ts
+++
@@ -1,3 +0,0 @@
-export function formatCurrency(amount: string, currency = "USD") {
-  return `$${amount.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`;
-}
--- apps/dashboard/src/lib/resend 2.ts
+++
@@ -1,4 +0,0 @@
-import { Resend } from "resend";
-import { env } from "@hr-toolkit/env";
-
-export const resend = new Resend(env.NEXT_PUBLIC_RESEND_API_KEY);
